b9d288d1e5d38433b75ccd400cb6e982189a2e47
759071793 <759071793@qq.com>
2022-01-16 19:47:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git Lienol https://github.com/Lienol/openwrt-package.git' >>feeds.conf.default
